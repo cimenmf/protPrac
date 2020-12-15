67220a30cc6dd2de5ac97dbef9ce64d00b45f7a3
feyyaz <cimenmf@gmail.com>
2020-12-14 21:30:17 -0500

    homework_in_progress

--- Homework.js
+++ Homework.js
@@ -25,10 +25,13 @@ describe('Locating Elements', function(){
 		element(by.linkText("Shop")).click();
 		
 		//use the funtion instead of the code above
-		var itemList= ["Samsung Note 8","iphone X","Blackberry"];
+		var itemList= ["Samsung Note 8","Nokia Edge","Blackberry"];
+		selectItem(itemList[0]);
+		selectItem(itemList[1]);
+		selectItem(itemList[1]);
 		selectItem(itemList[1]);
 		selectItem(itemList[2]);
-		selectItem(itemList[0]);
+		selectItem(itemList[2]);
 		var len = itemList.length;
 		element(by.partialLinkText("Checkout")).getText().then(function(text){
 			console.log(text);
@@ -37,27 +40,91 @@ describe('Locating Elements', function(){
 			var resultArr	= text.split("(");
 	        var num =  Number(resultArr[1].trim().charAt(0));
 			console.log("number of items in cart: " + num);
-			expect(num).toBe(len) ;
+			
+			expect    (num) .   toBe   (6) ;
+		
+		
 		})
 
-		
-		//go to cart and verify the prices and items amount and total price.
-				
+		var totalPriceCalculated;
+		var totalCaptured;
+		//go to cart and verify the item names				
 //	      element(by.partialLinkText("Checkout")).click();
 		  element(by.className("nav-link btn btn-primary")).click();
 			
-		element.all(by.css("table tbody tr h4") ).each(function(item){
-				console.log( item.element(by.css("a:nth-child(1)")).getText());
+		//to be able to get the text or something from browser we must resolve the promise
+		// ( using  getText().then(function(){} ) 
+		
+			element.all(by.css("h4[class='media-heading']") ).each(function(item){
+				console.log("---------------------------------------------");
+				item.element(by.css("h4 a")).getText().then(function(text){
+				 
+				 console.log(text);
+				 
+				 if(itemList.includes(text)){
+					 console.log("the list match: "+ text );
+				 } 
+			 })
 		})
 		
-		//first make sure the list of the item names in the cart is matching with the list of the items sent by user.
+		
+		//get the names of each item, simTitle and status
+		
+		element.all(by.css("table tbody tr")).each(function(row) {
+			console.log("---------------------------------------------");
+			row.element(by.css("td:nth-child(1)")).getText().then(function(textOfColumsInRows){
+				console.log("brand: "+textOfColumsInRows);
+			})
+			// td:nth-child(4) strong
+		})
 		
 		
+
+		
+		//get the names of each item's price
+			
+			element.all(by.css("table tbody tr")).each(function(row) {
+				console.log("---------------------------------------------");
+				row.element(by.css("td:nth-child(3)")).getText().then(function(textOfColumsInRows){
+				
+					console.log("price: "+textOfColumsInRows);
+				})
+				// td:nth-child(4) strong
+			})
+			
+			
+			//get the names of each item's total price
+			
+			element.all(by.css("table tbody tr")).each(function(row) {
+				console.log("---------------------------------------------");
+				row.element(by.css("td:nth-child(4)")).getText().then(function(textOfColumsInRows){
+					
+					console.log("pure quantity: "+ textOfColumsInRows);
+					
+					var resultArr = textOfColumsInRows.split(".");
+					var newStr= resultArr[1].substring(1);
+			        var num =  Number(newStr );
+					console.log("trimmed quantity: "+ num);
+					totalPriceCalculated += num;
+					
+					console.log("current total: " +totalPriceCalculated);
+				})
+				// td:nth-child(4) strong
+			})
+			
+			element(by.css("table tbody tr td:nth-child(5) h3")).getText().then(function(total){
+				 console.log("total Captured: "  + total);
+				 totalCaptured = Number(total);
+				 console.log("total Captured: "  + totalCaptured);
+				 console.log("total Calculated:" + totalPriceCalculated);
+//				 browser.sleep(15000);
+//				 expect   ( totalPriceCalculated)  .   toBe   (totalCaptured)   ;
+				 
+				 
+			})
 		
 		
-		
-		
-		
+			
 	})
 })
 
